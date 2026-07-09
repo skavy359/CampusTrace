04ee4715eab9de6aaf52f57e1d12cedbb21c773a
Kavy Sharma <skavy359@gmail.com>
2026-07-09 10:01:34 +0530

    Add custom exception for resource not found

---
+++ user-service/src/main/java/com/example/userservice/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.userservice.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
